ceee2c74afefd8558ef9a88191db5909d7924807
Abdulqodir <abdulqodirmaxmadaliyev03@gmail.com>
2023-05-06 11:50:05 +0500

    initial commit

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("#login-form"); // login form elementini qidirish
+const emailInput = document.querySelector("#email-input"); // e-mail input elementini qidirish
+const passwordInput = document.querySelector("#password-input"); // parol input elementini qidirish
+
+loginForm.addEventListener("submit", async (event) => {
+
+    event.preventDefault(); 
+
+    const email = emailInput.value;
+    const password = passwordInput.value; 
+
+    try {
+        const response = await fetch("https://fakestoreapi.com/users", {
+            
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+        });
+        if (!response.ok) {
+            
+            throw new Error("Login yoki parol xato");
+        }
+
+        const data = await response.json(); 
+        const token = data.token; 
+
+        localStorage.setItem("token", token);
+
+        window.location.href = "http://127.0.0.1:5501/index.html#";
+    } catch (error) {
+        console.error(error);
+    }
+});
